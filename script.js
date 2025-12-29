a65762fe34beb3b1dc511f390c63024e71e0f9a1
Sunay16018 <seyidlisunay20@gmail.com>
2025-12-29 17:25:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const socket = io();
 let mOn = false;
 
-function start() {
+function start(isMulti) {
     const host = document.getElementById('host').value;
     const user = document.getElementById('user').value;
-    const pass = document.getElementById('pass').value;
     if(!host || !user) return alert("Bilgileri doldur!");
-    
-    document.getElementById('s-btn').innerText = "BAĞLANILIYOR...";
-    socket.emit('start-bot', { host, username: user, password: pass });
+    socket.emit('start-bot', { host, username: user, isMulti });
 }
 
 function toggleM() {
@@ -24,36 +21,29 @@ function sync() {
     if(user) socket.emit('update-settings', { user, mathOn: mOn, mathSec: delay });
 }
 
+function move(dir, type) {
+    const user = document.getElementById('bot-sel').value;
+    if(user) socket.emit('move-bot', { username: user, dir, type });
+}
+
 socket.on('status', (d) => {
-    document.getElementById('s-btn').innerText = "SİSTEMİ BAŞLAT";
     if (d.connected && !document.getElementById(`b-${d.username}`)) {
-        document.getElementById('active-list').innerHTML += `<div class="card" id="b-${d.username}">${d.username} <button onclick="socket.emit('stop-bot','${d.username}')">X</button></div>`;
+        document.getElementById('bot-list').innerHTML += `<div class="card" id="b-${d.username}">${d.username}</div>`;
         let o = document.createElement('option'); o.value = d.username; o.innerText = d.username; o.id = `o-${d.username}`;
         document.getElementById('bot-sel').appendChild(o);
-    } else if(!d.connected) {
-        document.getElementById(`b-${d.username}`)?.remove();
-        document.getElementById(`o-${d.username}`)?.remove();
     }
 });
 
 socket.on('log', (d) => {
     const l = document.getElementById('logs');
-    const p = document.createElement('div');
-    p.className = 'msg-line';
-    p.innerHTML = `<span class="u">[${d.username}]</span> ${d.msg}`;
-    l.appendChild(p);
+    l.innerHTML += `<p><b>[${d.username}]</b> ${d.msg}</p>`;
     l.scrollTop = l.scrollHeight;
 });
 
-function move(dir) {
-    const u = document.getElementById('bot-sel').value;
-    if(u) socket.emit('move-bot', { username: u, dir });
-}
-
 document.getElementById('chat-in').onkeydown = (e) => {
-    if (e.key === 'Enter' && e.target.value) {
-        const u = document.getElementById('bot-sel').value;
-        socket.emit('send-chat', { username: u, msg: e.target.value });
+    if (e.key === 'Enter') {
+        const user = document.getElementById('bot-sel').value;
+        socket.emit('send-chat', { username: user, msg: e.target.value });
         e.target.value = '';
     }
 };
