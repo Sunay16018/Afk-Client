bdc1f2f371fb6f071ad06fb8f09d8015966001e9
Sunay16018 <seyidlisunay20@gmail.com>
2025-12-29 21:08:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,45 +3,41 @@ let selBot = "";
 const el = (i) => document.getElementById(i);
 
 function connect() {
-    socket.emit('start-bot', { 
-        host: el('ip').value, 
-        username: el('nick').value, 
-        pass: el('pass').value 
-    });
+    socket.emit('start-bot', { host: el('ip').value, username: el('nick').value, pass: el('pass').value });
 }
 
-function disconnect() { 
-    if(selBot) socket.emit('quit', selBot); 
-}
-
-function move(dir) { 
-    if(selBot) socket.emit('move', { user: selBot, dir }); 
-}
-
+function disconnect() { if(selBot) socket.emit('quit', selBot); }
+function move(dir) { if(selBot) socket.emit('move', { user: selBot, dir }); }
 function openSet() { el('modal').style.display = 'flex'; }
-
 function save() {
-    const config = { 
-        math: el('m-on').checked, 
-        delay: el('m-del').value, 
-        mine: el('mine-on').checked 
-    };
-    socket.emit('update-config', { user: selBot, config });
+    socket.emit('update-config', { user: selBot, config: { math: el('m-on').checked, mine: el('mine-on').checked } });
     el('modal').style.display = 'none';
 }
+
+// MESAJ GÖNDERME FONKSİYONU
+function sendChat() {
+    const msg = el('cin').value;
+    if(selBot && msg.trim() !== "") {
+        socket.emit('chat', { user: selBot, msg: msg });
+        el('cin').value = "";
+    }
+}
+
+// Enter tuşu ile gönderme
+el('cin').onkeydown = (e) => {
+    if(e.key === 'Enter') sendChat();
+};
 
 socket.on('status', d => {
     const s = el('bot-sel');
     if (d.online) {
         if (!el("opt-"+d.user)) {
-            let o = document.createElement('option');
-            o.value = d.user; o.id = "opt-"+d.user; o.innerText = d.user;
+            let o = document.createElement('option'); o.value = d.user; o.id = "opt-"+d.user; o.innerText = d.user;
             s.appendChild(o);
         }
         selBot = d.user; s.value = d.user;
     } else {
-        const o = el("opt-"+d.user);
-        if(o) o.remove();
+        const o = el("opt-"+d.user); if(o) o.remove();
         selBot = s.value;
     }
 });
@@ -51,11 +47,3 @@ socket.on('log', d => {
     l.innerHTML += `<div><span style="color:#00ffcc">></span> ${d.msg}</div>`;
     l.scrollTop = l.scrollHeight;
 });
-
-// Chat Gönderme Aktif
-el('cin').onkeydown = e => {
-    if(e.key === 'Enter' && selBot && e.target.value.trim() !== "") {
-        socket.emit('chat', { user: selBot, msg: e.target.value });
-        e.target.value = "";
-    }
-};
