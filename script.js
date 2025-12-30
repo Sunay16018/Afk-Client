95546b3e37ddab4e1655d2bf05abb2cd10b83f0d
Sunay16018 <seyidlisunay20@gmail.com>
2025-12-30 16:19:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
+const s = io();
+const logBox = document.getElementById("log");
+const settings = document.getElementById("settings");
+const Convert = new AnsiConvert();
 
-const s=io();
-const logBox=document.getElementById("log");
-const settings=document.getElementById("settings");
+function log(m){
+ try{
+   let obj = JSON.parse(m);
+   if(obj.text) m = obj.text;
+ }catch(e){}
+ logBox.innerHTML += Convert.toHtml(m) + "<br>";
+ logBox.scrollTop = logBox.scrollHeight;
+}
 
-function log(m){logBox.innerHTML+=m+"<br>";logBox.scrollTop=logBox.scrollHeight;}
-s.on("log",log);
+s.on("log", log);
 
-settingsBtn.onclick=()=>settings.classList.toggle("hidden");
+settingsBtn.onclick = ()=>settings.classList.toggle("hidden");
 
-start.onclick=()=>s.emit("startBot",{
- ip:ip.value,port:port.value,username:username.value,
- password:password.value,version:version.value
+start.onclick = ()=>s.emit("startBot",{
+ ip: ip.value, port: port.value, username: username.value,
+ password: password.value, version: version.value
 });
 
-stop.onclick=()=>s.emit("stopBot",username.value);
+stop.onclick = ()=>s.emit("stopBot", username.value);
 
-send.onclick=()=>{
- s.emit("sendChat",{username:username.value,message:chatMsg.value});
- chatMsg.value="";
+send.onclick = ()=>{
+ s.emit("sendChat",{username: username.value, message: chatMsg.value});
+ chatMsg.value = "";
 };
 
-autoMsgEnable.onchange=()=>{
+autoMsgEnable.onchange = ()=>{
  s.emit("autoMsg",{
-  username:username.value,
-  enabled:autoMsgEnable.checked,
-  message:autoMsgText.value,
-  delay:Number(autoMsgDelay.value||5)
+  username: username.value,
+  enabled: autoMsgEnable.checked,
+  message: autoMsgText.value,
+  delay: Number(autoMsgDelay.value||5)
  });
 };
 
 document.querySelectorAll(".controls button").forEach(b=>{
- const k=b.dataset.k;
- b.onmousedown=()=>s.emit("move",{username:username.value,key:k,state:true});
- b.onmouseup=()=>s.emit("move",{username:username.value,key:k,state:false});
- b.ontouchstart=()=>s.emit("move",{username:username.value,key:k,state:true});
- b.ontouchend=()=>s.emit("move",{username:username.value,key:k,state:false});
+ const k = b.dataset.k;
+ b.onmousedown = ()=>s.emit("move",{username: username.value, key: k, state: true});
+ b.onmouseup = ()=>s.emit("move",{username: username.value, key: k, state: false});
+ b.ontouchstart = ()=>s.emit("move",{username: username.value, key: k, state: true});
+ b.ontouchend = ()=>s.emit("move",{username: username.value, key: k, state: false});
 });
