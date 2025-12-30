c51fdf1cacca2d23f94986014e94fec09ad79476
Sunay16018 <seyidlisunay20@gmail.com>
2025-12-31 01:03:19 +0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 let sessions = {}; 
 let logs = {}; 
+let configs = {}; 
 
 function startBot(sid, host, user, ver) {
     if (!sessions[sid]) sessions[sid] = {};
@@ -22,24 +23,22 @@ function startBot(sid, host, user, ver) {
         username: user, 
         version: ver, 
         auth: 'offline',
-        hideErrors: true // Gereksiz hata mesajlarını gizle, botu yormasın
+        // Bağlantıyı stabil tutan ayarlar
+        checkTimeoutInterval: 60000,
+        physicsEnabled: true // Bazı sunucular hareket bekler
     });
 
     sessions[sid][user] = bot;
+    configs[key] = { msgT: null, clickT: null, mining: false };
 
     bot.on('login', () => {
-        logs[key].push("<b style='color:#2ecc71'>BAŞARIYLA GİRDİ!</b>");
+        logs[key].push("<b style='color:#2ecc71'>GİRİŞ YAPILDI!</b>");
     });
 
-    // Sunucudan atılma sebebini en net şekilde oku
     bot.on('kicked', (reason) => {
-        let msg = "Atıldı.";
-        try {
-            msg = typeof reason === 'string' ? reason : JSON.stringify(reason);
-            if (reason.extra) msg = reason.extra.map(e => e.text).join("");
-            else if (reason.text) msg = reason.text;
-        } catch(e) {}
-        logs[key].push("<b style='color:#e74c3c'>SUNUCU ATTI: " + msg + "</b>");
+        let msg = reason;
+        try { if(reason.extra) msg = reason.extra.map(e => e.text).join(""); else if(reason.text) msg = reason.text; } catch(e){}
+        logs[key].push("<b style='color:#e74c3c'>ATILDI: " + msg + "</b>");
         delete sessions[sid][user];
     });
 
@@ -50,7 +49,7 @@ function startBot(sid, host, user, ver) {
 
     bot.on('end', () => {
         if (sessions[sid]?.[user]) {
-            logs[key].push("<b style='color:gray'>BAĞLANTI SONLANDI.</b>");
+            logs[key].push("<b style='color:gray'>BAĞLANTI KESİLDİ.</b>");
             delete sessions[sid][user];
         }
     });
@@ -61,7 +60,6 @@ function startBot(sid, host, user, ver) {
     });
 }
 
-// HTTP İletişim Hattı
 http.createServer((req, res) => {
     const q = url.parse(req.url, true).query;
     const p = url.parse(req.url, true).pathname;
@@ -70,11 +68,32 @@ http.createServer((req, res) => {
     if (p === '/start' && sid) { startBot(sid, q.host, q.user, q.ver); return res.end("ok"); }
     
     if (p === '/send' && sid) {
-        if (sessions[sid]?.[q.user]) {
-            sessions[sid][q.user].chat(decodeURIComponent(q.msg));
-            return res.end("ok");
-        }
+        const b = sessions[sid]?.[q.user];
+        if (b) { b.chat(decodeURIComponent(q.msg)); return res.end("ok"); }
         return res.end("error");
+    }
+
+    if (p === '/update' && sid) {
+        const key = sid + "_" + q.user;
+        const b = sessions[sid]?.[q.user];
+        const c = configs[key];
+        if (!b || !c) return res.end("offline");
+        
+        clearInterval(c.msgT); clearInterval(c.clickT);
+        if (q.type === 'msg' && q.status === 'on') c.msgT = setInterval(() => b.chat(q.val), q.sec * 1000);
+        else if (q.type === 'click' && q.status === 'on') {
+            c.clickT = setInterval(() => { b.activateItem(); const bl = b.blockAtCursor(4); if (bl) b.activateBlock(bl).catch(()=>{}); }, q.sec * 1000);
+        }
+        else if (q.type === 'mining' && q.status === 'on') {
+            c.mining = true;
+            const dig = async () => {
+                if (!configs[key]?.mining || !sessions[sid]?.[q.user]) return;
+                const t = sessions[sid][q.user].blockAtCursor(4);
+                if (t && t.type !== 0) { try { await sessions[sid][q.user].lookAt(t.position, true); await sessions[sid][q.user].dig(t, true); } catch(e) {} }
+                setTimeout(dig, 500);
+            }; dig();
+        } else if (q.type === 'mining' && q.status === 'off') { c.mining = false; }
+        return res.end("ok");
     }
 
     if (p === '/data' && sid) {
@@ -88,3 +107,4 @@ http.createServer((req, res) => {
     let f = path.join(__dirname, p === '/' ? 'index.html' : p);
     fs.readFile(f, (err, data) => res.end(data));
 }).listen(process.env.PORT || 10000);
+            
