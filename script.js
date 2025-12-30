04d9be18e9111c92bffec378247a34a776c747b3
Sunay16018 <seyidlisunay20@gmail.com>
2025-12-30 15:06:35 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,34 +2,28 @@ const socket = io();
 let selBot = "";
 const el = (id) => document.getElementById(id);
 
-function toggleModal(id, show) { el(id).style.display = show ? 'flex' : 'none'; }
+function openM(id) { el(id).style.display = 'flex'; }
+function closeM(id) { el(id).style.display = 'none'; }
 
 function connect() {
-    const data = { host: el('ip').value, username: el('nick').value, pass: el('pass').value };
-    if(!data.host || !data.username) return;
-    socket.emit('start-bot', data);
-    toggleModal('bot-modal', false);
+    socket.emit('start-bot', { host: el('ip').value, username: el('nick').value, pass: el('pass').value });
+    closeM('bot-m');
 }
 
 function disconnect() { if(selBot) socket.emit('quit', selBot); }
 
-function saveSettings() {
+function save() {
     if(!selBot) return;
-    const config = {
-        autoRevive: el('rev-on').checked,
-        math: el('math-on').checked,
-        autoMsg: el('msg-on').checked,
-        msgText: el('msg-text').value,
-        msgDelay: parseInt(el('msg-sec').value) || 30
-    };
-    socket.emit('update-config', { user: selBot, config });
-    toggleModal('set-modal', false);
+    socket.emit('update-config', { user: selBot, config: {
+        autoRevive: el('rev-on').checked, math: el('math-on').checked,
+        msgText: el('msg-t').value, msgDelay: parseInt(el('msg-s').value) || 30, autoMsg: !!el('msg-t').value
+    }});
+    closeM('set-m');
 }
 
 function sendChat() {
-    const msg = el('cin').value.trim();
-    if(selBot && msg) {
-        socket.emit('chat', { user: selBot, msg: msg });
+    if(selBot && el('cin').value) {
+        socket.emit('chat', { user: selBot, msg: el('cin').value });
         el('cin').value = "";
     }
 }
@@ -51,8 +45,7 @@ socket.on('status', d => {
 socket.on('log', d => {
     const l = el('logs');
     const div = document.createElement('div');
-    div.style.marginBottom = "4px";
-    div.innerHTML = `<span style="color:#777">[${d.user}]</span> ${d.msg}`;
+    div.innerHTML = `<span style="color:#888">[${d.user}]</span> ${d.msg}`;
     l.appendChild(div);
     l.scrollTop = l.scrollHeight;
 });
