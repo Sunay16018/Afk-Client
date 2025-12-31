efec153dc0651d98b85d3f7fe41276d77f595364
Sunay16018 <seyidlisunay20@gmail.com>
2025-12-31 17:08:37 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-const Vec3 = require('vec3');
 
 let sessions = {}; 
 let logs = {}; 
@@ -15,7 +14,7 @@ function startBot(sid, host, user, ver) {
 
     const key = sid + "_" + user;
     const [ip, port] = host.split(':');
-    logs[key] = ["<b style='color:gray'>[SİSTEM] Bot başlatılıyor...</b>"];
+    logs[key] = ["<b style='color:gray'>[SİSTEM] Başlatılıyor...</b>"];
 
     const bot = mineflayer.createBot({
         host: ip, port: parseInt(port) || 25565, 
@@ -23,14 +22,11 @@ function startBot(sid, host, user, ver) {
     });
 
     sessions[sid][user] = bot;
-    configs[key] = { mining: false, jump: null, rclick: null };
+    configs[key] = { mining: null, jump: null, rclick: null };
 
     bot.on('login', () => logs[key].push("<b style='color:#2ecc71'>[GİRİŞ] Başarılı!</b>"));
     bot.on('kicked', (r) => logs[key].push("<b style='color:#ff4757'>[ATILDI] " + r + "</b>"));
-    bot.on('end', () => { 
-        logs[key].push("<b style='color:#f39c12'>[BAĞLANTI KESİLDİ]</b>"); 
-        delete sessions[sid][user]; 
-    });
+    bot.on('end', () => { logs[key].push("<b style='color:#f39c12'>[BAĞLANTI KESİLDİ]</b>"); delete sessions[sid][user]; });
     bot.on('error', (e) => logs[key].push("<b style='color:#ff4757'>[HATA] " + e.message + "</b>"));
 
     bot.on('message', (m) => {
@@ -52,54 +48,65 @@ http.createServer((req, res) => {
     if (p === '/update' && bot) {
         const key = sid + "_" + q.user;
         const conf = configs[key];
+        const sec = parseInt(q.sec) * 1000 || 2000; // Varsayılan 2 saniye
 
-        // YÖN DEĞİŞTİRME - KESİN ÇÖZÜM (lookAt ve Koordinat Hesaplama)
+        // YÖN KONTROLÜ (90 Derece Kesin Dönüş)
         if (q.type === 'look') {
-            const pos = bot.entity.position;
-            if (q.dir === 'left') bot.look(bot.entity.yaw + Math.PI / 2, bot.entity.pitch, true);
-            if (q.dir === 'right') bot.look(bot.entity.yaw - Math.PI / 2, bot.entity.pitch, true);
-            if (q.dir === 'up') bot.look(bot.entity.yaw, Math.PI / 2, true);
-            if (q.dir === 'down') bot.look(bot.entity.yaw, -Math.PI / 2, true);
-            if (q.dir === 'front') bot.look(bot.entity.yaw, 0, true);
+            let yaw = bot.entity.yaw;
+            let pitch = bot.entity.pitch;
+            if (q.dir === 'left') yaw += Math.PI / 2;
+            if (q.dir === 'right') yaw -= Math.PI / 2;
+            if (q.dir === 'up') pitch = Math.PI / 2;
+            if (q.dir === 'down') pitch = -Math.PI / 2;
+            if (q.dir === 'front') pitch = 0;
+            bot.look(yaw, pitch, true);
         }
 
-        // AKILLI KAZMA (WAIT UNTIL BROKEN)
+        // ZAMAN AYARLI KAZMA (Her X saniyede bir resetler)
         if (q.type === 'mining') {
+            clearInterval(conf.mining);
             if (q.status === 'on') {
-                if (conf.mining) return res.end("ok");
-                conf.mining = true;
-                const mineLoop = async () => {
-                    while(conf.mining && sessions[sid]?.[q.user]) {
-                        const block = bot.blockAtCursor(4);
-                        if (block && block.name !== 'air') {
-                            try {
-                                await bot.dig(block, true); // true: kazarken başka yere bakma
-                            } catch (e) { await new Promise(r => setTimeout(r, 100)); }
-                        } else {
-                            await new Promise(r => setTimeout(r, 200));
-                        }
+                conf.mining = setInterval(() => {
+                    const block = bot.blockAtCursor(4);
+                    if (block && block.name !== 'air') {
+                        // Kazma işlemini başlat (Hata alsa bile 2 sn sonra tekrar dener)
+                        bot.dig(block, 'ignore').catch(() => {});
                     }
-                };
-                mineLoop();
+                }, sec);
             } else {
-                conf.mining = false;
                 bot.stopDigging();
             }
         }
 
-        // ZIPLAMA & SAĞ TIK
-        const sec = parseInt(q.sec) * 1000 || 1000;
-        if (q.type === 'jump') {
-            clearInterval(conf.jump);
-            if (q.status === 'on') conf.jump = setInterval(() => { bot.setControlState('jump', true); setTimeout(() => bot.setControlState('jump', false), 400); }, sec);
-        }
+        // SAĞ TIK (Bakışı bozmadan tıklama)
         if (q.type === 'rclick') {
             clearInterval(conf.rclick);
-            if (q.status === 'on') conf.rclick = setInterval(() => bot.activateItem(), sec);
+            if (q.status === 'on') {
+                conf.rclick = setInterval(() => {
+                    const block = bot.blockAtCursor(4);
+                    if (block) {
+                        // Hem bloğa sağ tıkla hem elindeki eşyayı kullan
+                        bot.activateBlock(block).catch(() => {});
+                        bot.activateItem();
+                    } else {
+                        bot.activateItem();
+                    }
+                }, sec);
+            }
+        }
+
+        // ZIPLAMA
+        if (q.type === 'jump') {
+            clearInterval(conf.jump);
+            if (q.status === 'on') {
+                conf.jump = setInterval(() => {
+                    bot.setControlState('jump', true);
+                    setTimeout(() => bot.setControlState('jump', false), 400);
+                }, sec);
+            }
         }
 
         if (q.type === 'drop' && bot.heldItem) bot.tossStack(bot.heldItem);
-        
         return res.end("ok");
     }
 
