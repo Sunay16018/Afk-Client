c279375e0f5904117560ef811abe8a4caea8be5b
Sunay16018 <seyidlisunay20@gmail.com>
2025-12-31 14:01:31 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
-let sessions = {}; // Yapı: { "tarayici_id": { "bot_nick": bot_objesi } }
+let sessions = {}; 
 let logs = {}; 
 let configs = {}; 
 
@@ -14,7 +14,7 @@ function startBot(sid, host, user, ver) {
 
     const key = sid + "_" + user;
     const [ip, port] = host.split(':');
-    logs[key] = ["<b style='color:gray'>Bağlantı isteği gönderildi...</b>"];
+    logs[key] = ["<b style='color:gray'>Bağlanılıyor...</b>"];
 
     const bot = mineflayer.createBot({
         host: ip, port: parseInt(port) || 25565, 
@@ -24,20 +24,17 @@ function startBot(sid, host, user, ver) {
     sessions[sid][user] = bot;
     configs[key] = { msgT: null, afkT: null, mining: false };
 
-    bot.on('login', () => logs[key].push("<b style='color:#2ecc71'>Bot oyuna girdi!</b>"));
-
+    bot.on('login', () => logs[key].push("<b style='color:#2ecc71'>[SİSTEM] Bot Giriş Yaptı!</b>"));
+    
     bot.on('kicked', (reason) => {
-        logs[key].push("<b style='color:#ff4757'>Atıldı: Sunucu bağlantıyı kesti.</b>");
+        let msg = typeof reason === 'string' ? reason : JSON.stringify(reason);
+        try { if(reason.extra) msg = reason.extra.map(e => e.text).join(""); } catch(e){}
+        logs[key].push("<b style='color:#ff4757'>[ATILDI] " + msg + "</b>");
         delete sessions[sid][user];
     });
 
     bot.on('error', (err) => {
-        logs[key].push("<b style='color:#ff4757'>Hata: " + err.message + "</b>");
-        delete sessions[sid][user];
-    });
-
-    bot.on('end', () => {
-        logs[key].push("<b style='color:#ffa502'>Bağlantı sonlandırıldı.</b>");
+        logs[key].push("<b style='color:#ff4757'>[HATA] " + err.message + "</b>");
         delete sessions[sid][user];
     });
 
@@ -50,12 +47,12 @@ function startBot(sid, host, user, ver) {
 http.createServer((req, res) => {
     const q = url.parse(req.url, true).query;
     const p = url.parse(req.url, true).pathname;
-    const sid = q.sid; // Tarayıcıdan gelen özel kimlik
+    const sid = q.sid;
+    const key = sid + "_" + q.user;
     const bot = sessions[sid]?.[q.user];
 
     if (p === '/start' && sid) { startBot(sid, q.host, q.user, q.ver); return res.end("ok"); }
     
-    // ANINDA BAĞLANTIYI KES
     if (p === '/stop' && bot) {
         bot.quit();
         delete sessions[sid][q.user];
@@ -68,21 +65,27 @@ http.createServer((req, res) => {
     }
 
     if (p === '/update' && bot) {
-        const key = sid + "_" + q.user;
         const conf = configs[key];
+        // OTO MESAJ
         if (q.type === 'msg') {
             clearInterval(conf.msgT);
-            if (q.status === 'on') conf.msgT = setInterval(() => bot.chat(decodeURIComponent(q.val)), q.sec * 1000);
-        } else if (q.type === 'afk') {
+            if (q.status === 'on') {
+                conf.msgT = setInterval(() => bot.chat(decodeURIComponent(q.val)), q.sec * 1000);
+            }
+        } 
+        // ANTI-AFK
+        else if (q.type === 'afk') {
             clearInterval(conf.afkT);
             if (q.status === 'on') {
                 conf.afkT = setInterval(() => {
-                    bot.look(Math.random() * 6, (Math.random() - 0.5));
+                    bot.look(Math.random() * 6.2, (Math.random() - 0.5));
                     bot.setControlState('jump', true);
                     setTimeout(() => bot.setControlState('jump', false), 400);
                 }, 10000);
             }
-        } else if (q.type === 'mining') {
+        } 
+        // KAZMA
+        else if (q.type === 'mining') {
             if (q.status === 'on') {
                 conf.mining = true;
                 const mine = async () => {
@@ -109,4 +112,3 @@ http.createServer((req, res) => {
     let f = path.join(__dirname, p === '/' ? 'index.html' : p);
     fs.readFile(f, (err, data) => res.end(data));
 }).listen(process.env.PORT || 10000);
-                
